ec054576c75bfdd93a954d3cfc45ba6ea98d7abd
ezeaniiandrew <ezeaniiandrew@gmail.com>
2026-04-20 21:19:21 +0100

    feat: create reusable TextField component

---
+++ src/components/ui/TextField.tsx
@@ -0,0 +1,24 @@
+import { type InputHTMLAttributes } from "react";
+import styles from "./TextField.module.css";
+
+export interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  id: string;
+  className?: string;
+}
+
+export function TextField({
+  label,
+  id,
+  className = "",
+  ...props
+}: TextFieldProps) {
+  return (
+    <div className={`${styles.container} ${className}`}>
+      <label htmlFor={id} className={styles.label}>
+        {label}
+      </label>
+      <input id={id} className={styles.input} {...props} />
+    </div>
+  );
+}
