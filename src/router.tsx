ebfd068fc5426ca9583b6d415d3811da979e3422
ezeaniiandrew <ezeaniiandrew@gmail.com>
2026-04-21 19:15:15 +0100

    feat: add nested routes for home and invoice details pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,20 @@
-import { createBrowserRouter } from 'react-router'
-import App from './App'
+import { createBrowserRouter } from 'react-router';
+import App from './App';
+import { Home } from './pages/Home';
+import { InvoiceDetails } from './pages/InvoiceDetails';
 
 export const router = createBrowserRouter(
   [
     {
       path: "/",
       element: <App />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "invoice/:id", element: <InvoiceDetails /> }
+      ]
     },
   ],
   {
     basename: import.meta.env.BASE_URL,
   }
-)
+);
