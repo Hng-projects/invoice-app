2b2243868febfa59d01eaf66f5045bc7d28a9691
ezeaniiandrew <ezeaniiandrew@gmail.com>
2026-04-21 01:48:41 +0100

    feat: add invoiceCount prop for tracking invoice qty and make header subtitle more dynamic

--- src/components/invoices/InvoicesHeader/index.tsx
+++ src/components/invoices/InvoicesHeader/index.tsx
@@ -3,17 +3,30 @@ import { Button } from "../../ui/Button";
 import { Filter } from "../Filter";
 import styles from "./invoices-header.module.css";
 
-export function InvoicesHeader() {
+interface InvoicesHeaderProps {
+  invoiceCount: number;
+}
+
+export function InvoicesHeader({ invoiceCount }: InvoicesHeaderProps) {
   return (
     <header className={styles.header}>
       <div className={styles.left}>
         <h1 className={styles.title}>Invoices</h1>
-        <p className={styles.subtitle}>No invoices</p>
+        <p className={styles.subtitle}>
+          {invoiceCount === 0 ? (
+            "No invoices"
+          ) : (
+            <>
+              <span className={styles.mobileSubtitle}>{invoiceCount} invoices</span>
+              <span className={styles.desktopSubtitle}>There are {invoiceCount} total invoices</span>
+            </>
+          )}
+        </p>
       </div>
       <div className={styles.right}>
         <Filter />
         <Button icon={<FaPlus size={12} />} onClick={() => console.log("New Invoice")}>
-          New <span className={styles.buttonText}>Invoice</span>
+          New<span className={styles.buttonText}>{" "}Invoice</span>
         </Button>
       </div>
     </header>
