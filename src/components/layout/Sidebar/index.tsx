7a86f857c10dba4e81bda3a1b6c2573a1c64078e
ezeaniiandrew <ezeaniiandrew@gmail.com>
2026-04-23 03:22:23 +0100

    feat: make logo a home navigation link

--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -2,6 +2,7 @@ import { FaMoon, FaSun } from "react-icons/fa";
 import styles from "./sidebar.module.css";
 import avatarImg from "../../../assets/avatar.png";
 import { useTheme } from "../../../contexts/ThemeContext";
+import { Link } from "react-router";
 
 export function Sidebar() {
   const { theme, toggleTheme } = useTheme();
@@ -9,13 +10,13 @@ export function Sidebar() {
 
   return (
     <aside className={styles.sidebar}>
-      <div className={styles.logoContainer}>
+      <Link to="/" className={styles.logoContainer}>
         <img
           src={`${import.meta.env.BASE_URL}logo.svg`}
           alt="Invoice App Logo"
           className={styles.logo}
         />
-      </div>
+      </Link>
 
       <div className={styles.actions}>
         <button
